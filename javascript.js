6bbb9e912bf0ade6231f9b6e0dd1e8c0fdc164b3
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-05 16:37:48 +0300

    Add Play Round Feature
    Mark a board spot from the prompt

--- javascript.js
+++ javascript.js
@@ -28,9 +28,23 @@ let player2 = player('Mark', '0');
 
 const gameController = function(){
     let activePlayer = player1;
+
     const switchPlayer = function(){
         if (activePlayer == player1){
             activePlayer = player2;
         }
     }
+
+    const board = gameBoard.board
+    const playRound = function() {
+            console.log(`It's Player1's turn. Please select board location to mark`);
+            row = prompt(`Board row location`, '0');
+            col = prompt(`Board column location`, '0');
+            board[row][col] = '1'
+    }
+    return{
+        playRound
+    }
 }
+
+let res = gameController()
